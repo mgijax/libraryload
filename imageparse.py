2bb9f0e052802da180f8e9a512d9d0cc98b309b1
Lori Corbani <Lori.Corbani@jax.org>
2003-03-11 14:19:38 +0000

    pre-JSAM

--- imageparse.py
+++ imageparse.py
@@ -51,7 +51,7 @@ CRT = '\n'
 
 NS = 'Not Specified'
 logicalDBName = 'I.M.A.G.E. Clone Libraries'
-jnum = 'J:80000'
+jnum = 'J:57656'
 createdBy = 'library_load'
 
 cdate = mgi_utils.date('%m/%d/%Y')	# current date
@@ -202,20 +202,86 @@ def processFile():
 		elif tokens[0] == 'STRAIN':
 
 			if len(token1) > 0:
-				strain = token1
+				if token1 == 'C3H x 101 (F1 stock)':
+					strain = '(C3H x 101)F1'
+				elif token1 == 'C57BL6 x DBA':
+					strain = 'C57BL/6 x DBA'
+				elif token1 == 'B6D2 F1/J':
+					strain = 'B6D2F1/J'
+				elif token1 == '129SV':
+					strain = '129/Sv'
+				elif token1 in ['CD-1', 'Inbred CD-1']:
+					strain = 'CD1'
+				elif token1 == 'NIH/Swiss':
+					strain = 'NIH'
+				elif token1 in ['C57/Bl6', 'C57/B6', 'C57/BL6', 'BL6']:
+					strain = 'C57BL/6'
+				elif token1 in ['CZECH II (feral)', 'CZECH II']:
+					strain = 'CZECHII'
+				elif token1 == '129 - C57/B6 - FVBN':
+					strain = '129 C57BL/6 FVB/N'
+				elif token1 == '129/Svx129/Sv-CP':
+					strain = '129/Sv x 129/Sv-p Tyr<c>'
+#				elif token1 == 'B5/EGFP transgenic ICR':
+#					strain = ''
+				else:
+					strain = token1
 
 		elif tokens[0] == 'SEX':
 
 			if len(token1) > 0:
-				gender = string.lower(token1)
+
+				if token1 in ['female', 'Female', 'female (lactating)', 'females', 'female, virgin']:
+					gender = 'Female'
+				elif token1 in ['male', 'Male', 'males']:
+					gender = 'Male'
+				elif token1 in ['pooled', 'mixed', 'both', 'equal ratio of male:female']:
+					gender = 'Pooled'
+				else:
+					gender = 'Not Specified'
 
 		elif tokens[0] == 'ORGAN':
 
 			if len(token1) > 0:
 				tissue = string.lower(token1)
 	
-				if tissue in ['colon cancer cell line', 'pituitary cell line']:
+				if tissue == 'testicles':
+					tissue = 'testis'
+
+				elif tissue == 'prostate':
+					tissue = 'prostate gland'
+
+				elif tissue in ['pancreas, pooled', 'whole pancreas, pooled', 'pool of whole pancreas, pancreatic bud, and islets of langerhans']:
+					tissue = 'pancreas'
+
+				elif tissue == 'pituitary cell line':
 					cellLine = tissue
+					tissue = 'pituitary gland'
+
+				elif tissue == 'colon cancer cell line':
+					cellLine = tissue
+					tissue = 'colon'
+
+				elif tissue in ['ovary, 6 and 10 hours post treatment with pmsg/hcg', 'ovary, 24 hours post treatment with pmsg/hcg', 'ovary, pmsg-treated']:
+					tissue = 'ovary'
+
+				elif tissue == 'mammary':
+					tissue = 'mammary gland'
+
+				elif tissue in ['inner ear, 170 pooled', 'otocysts']:
+					tissue = 'inner ear'
+
+				elif tissue == 'germinal b-cell':
+					tissue = 'B-lymphoblast'
+
+				elif tissue == 'bone (pooled)':
+					tissue = 'bone'
+
+				elif tissue == 'lymphocytes (flow-sorted)':
+					tissue = 'lymphocyte'
+
+				elif tissue == 'whole embryo':
+					tissue = 'embryo'
 
 		elif tokens[0] == 'STAGE':
 
